05eb2d11cb248bbe7831217eeeb1a9c207ad5630
xyizko <164354015+xyizko@users.noreply.github.com>
2024-10-31 01:25:14 +0000

    ⚙️Setup

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# xo-wgs
-🦧Ethereum Wallet Generator Script
+<p align="center"><a href="https://x.com/xyizko" target="_blank" rel="noopener noreferrer"><img src="https://raw.githubusercontent.com/xyizko/xo-tagz/refs/heads/main/gfx/a.png">
+</a></p>
+
+<p align="center"><a href="https://x.com/xyizko" target="_blank" rel="noopener noreferrer"><img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fxyizko%2Fxo-wgs&count_bg=%23142606&title_bg=%23610202&icon=&icon_color=%23E7E7E7&title=%F0%9F%A6%A7&edge_flat=false"/>
+</a></p>
+
+<p align="center"><a href="https://x.com/xyizko" target="_blank" rel="noopener noreferrer"><img src="./gfx/g.webp" width="500">
+</a></p>
+
+<h1 align="center"><code>xo-wgss</code></h1>
+<h2 align="center"><i> Single callable script to generate 10 ethereum wallets for TestNet </i></h2>
+
+[![uv](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/astral-sh/uv/main/assets/badge/v0.json)](https://github.com/astral-sh/uv)
+
+1. [💻 Compatibility](#-compatibility)
+2. [🎥 Demo](#-demo)
+3. [🤔What](#what)
+   1. [🍬 Features](#-features)
+4. [💽 Setup](#-setup)
+5. [⚠️ Warning - FOR TESTNET TRANSACTIONS ONLY](#️-warning---for-testnet-transactions-only)
+
+
+# 💻 Compatibility
+| Env                                                                          | Status |
+| ---------------------------------------------------------------------------- | ------ |
+| ![My Skills](https://skillicons.dev/icons?i=linux) | ✅     |
+
+
+# 🎥 Demo
+
+# 🤔What
+
+> Single callable python script that geenrated 10 ethereum testnet wallets. This script with [in-line metadata](https://docs.astral.sh/uv/guides/scripts/#running-a-script-with-dependencies) can be called directly form the cli. 
+
+## 🍬 Features
+
+✅ Can be called without downloading
+
+✅ Fast since this uses [uv](https://docs.astral.sh/uv/)
+
+# 💽 Setup
+
+Just execute the following command 
+
+```ml
+uv run --script w1.py
+```
+- A new `txt`file will be generated with the wallets
+
+# ⚠️ Warning - FOR TESTNET TRANSACTIONS ONLY
+
+![](https://media1.tenor.com/m/sLgNruA4tsgAAAAd/warning-lights.gif)
---
+++ xowgs.py
@@ -0,0 +1,123 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "eth-account",
+#     "logging",
+#     "requests",
+#     "rich",
+#     "web3",
+# ]
+# ///
+
+##########################################
+Source - https://github.com/xyizko/xo-wgs
+Pupose - Testnet ONLY !
+##########################################
+
+# --- Imports ---
+
+import logging
+import requests as rq
+from rich import print as rprint
+from rich.logging import RichHandler
+from rich.panel import Panel
+from rich.console import Console
+from rich.traceback import install
+from web3 import Web3
+from eth_account import Account
+from datetime import datetime
+
+# --- Import Configs ---
+
+# Tracebacks
+install(show_locals=True)
+
+# Initialize Console Function
+console = Console()
+
+# Configure logging
+logging.basicConfig(
+    level="DEBUG",
+    format="%(message)s",
+    datefmt="[%X]",
+    handlers=[RichHandler(rich_tracebacks=True, markup=True)],
+)
+logger = logging.getLogger(__name__)
+
+# Initialize Web3 instance (no need to connect to a provider just for generating wallets)
+w3 = Web3()
+
+
+# --- Functions ----
+
+
+# Banner Function
+def banr() -> str:
+    """
+    Function for grabbing the banner via curl and printing it
+    """
+    url = "https://snips.sh/f/ZuwtQ3Pk0x?r=1"
+    try:
+        r = rq.get(url)
+        r.raise_for_status()
+        banner_text = r.text
+        print(banner_text)  # Print the banner to the screen
+        return banner_text  # Return the banner text for later use
+    except rq.RequestException as e:
+        error_message = f"Failed to fetch banner. Error: {str(e)}"
+        console.print(f"[bold red]{error_message}[/bold red]")
+        return error_message
+
+
+# Warning Function
+def warning() -> None:
+    rprint(
+        Panel(
+            """[bold red]Generating 10 eth wallets strictly for testing purposes (TestNet). [/bold red]""",
+            title="[#87ff00][italic]Warning",
+            border_style="#d700d7",
+        )
+    )
+
+
+# Wallet Generation
+def generate_and_save_eth_wallets(num_wallets):
+    wallet_data = []
+
+    for _ in range(num_wallets):
+        account = Account.create()
+        wallet_info = {
+            "address": account.address,
+            "private_key": account._private_key.hex(),
+        }
+        wallet_data.append(wallet_info)
+
+    # Generate filename with current date and time
+    now = datetime.now()
+    filename = f"generated_wallets_{now.strftime('%Y%m%d_%H%M%S')}.txt"
+
+    # Write wallet data to the file
+    with open(filename, "w") as file:
+        for wallet in wallet_data:
+            file.write(f"Address: {wallet['address']}\n")
+            file.write(f"Private Key: {wallet['private_key']}\n")
+            file.write("\n")
+
+    # Log success message
+    logger.info(
+        f"[green] Success! {num_wallets} wallets have been generated and saved to {filename}[/green]"
+    )
+    console.line()
+    console.rule("[green]Success![/green]", style="green")
+
+
+# --- Main Function  ---
+def main() -> None:
+    print("Hello from w1.py!")
+    banr()
+    warning()
+    generate_and_save_eth_wallets(10)
+
+
+if __name__ == "__main__":
+    main()
